80cb12b33718c85ad55b8f0ff6514f7929906b93
abhiram chennupalli <abhiramchennupalli@gmail.com>
2026-04-27 11:58:28 +0530

    completed with out animations, small screen menu toggile not working and remainig side nav and almost over

--- sample.js
+++ sample.js
@@ -1,254 +1,289 @@
 document.getElementById("year").textContent = new Date().getFullYear();
+//ANIMATION
+// {
+//   //SECTION TWO ANIMATION
+//   {
+//     //HEADING
+//     {
+//       const elements = document.querySelectorAll(
+//         ".section-two-heading-animation",
+//       );
 
-//SECTION TWO ANIMATION
+//       let lastScrollY = window.scrollY;
+
+//       const observer = new IntersectionObserver(
+//         (entries) => {
+//           entries.forEach((entry) => {
+//             const currentScrollY = window.scrollY;
+
+//             if (entry.isIntersecting) {
+//               // Only animate when scrolling DOWN
+//               if (currentScrollY > lastScrollY) {
+//                 entry.target.classList.add("show");
+//               }
+//             } else {
+//               // Reset only when scrolling UP (so it can animate again later)
+//               if (currentScrollY < lastScrollY) {
+//                 entry.target.classList.remove("show");
+//               }
+//             }
+
+//             lastScrollY = currentScrollY;
+//           });
+//         },
+//         {
+//           threshold: 0.3,
+//         },
+//       );
+
+//       elements.forEach((el) => observer.observe(el));
+//     }
+
+//     {
+//       //SECOND SECTION CASE STUDY
+//       const elements = document.querySelectorAll(
+//         ".section-two-second-div-animation",
+//       );
+
+//       let lastScrollY = window.scrollY;
+
+//       const observer = new IntersectionObserver(
+//         (entries) => {
+//           entries.forEach((entry) => {
+//             const currentScrollY = window.scrollY;
+
+//             if (entry.isIntersecting) {
+//               // animate only when scrolling DOWN
+//               if (currentScrollY > lastScrollY) {
+//                 entry.target.classList.add("show");
+//               }
+//             } else {
+//               // reset when scrolling UP
+//               if (currentScrollY < lastScrollY) {
+//                 entry.target.classList.remove("show");
+//               }
+//             }
+
+//             lastScrollY = currentScrollY;
+//           });
+//         },
+//         {
+//           threshold: 0.3,
+//         },
+//       );
+
+//       elements.forEach((el) => observer.observe(el));
+//     }
+//   }
+
+//   //SECTION THREE ANIMATION
+//   {
+//     //1ST
+//     {
+//       const elements = document.querySelectorAll(".section-three-animation-one");
+
+//       let lastScrollY = window.scrollY;
+
+//       const observer = new IntersectionObserver(
+//         (entries) => {
+//           entries.forEach((entry) => {
+//             const currentScrollY = window.scrollY;
+
+//             if (entry.isIntersecting) {
+//               // Animate ONLY when scrolling DOWN
+//               if (currentScrollY > lastScrollY) {
+//                 entry.target.classList.add("show");
+//               }
+//             } else {
+//               // Reset silently when scrolling UP
+//               if (currentScrollY < lastScrollY) {
+//                 entry.target.classList.remove("show");
+//               }
+//             }
+
+//             lastScrollY = currentScrollY;
+//           });
+//         },
+//         {
+//           threshold: 0.3,
+//         },
+//       );
+
+//       elements.forEach((el) => observer.observe(el));
+//     }
+//     {
+//       //second
+//       const elements = document.querySelectorAll(".section-three-animation-one");
+
+//       let lastScrollY = window.scrollY;
+
+//       const observer = new IntersectionObserver(
+//         (entries) => {
+//           entries.forEach((entry) => {
+//             const currentScrollY = window.scrollY;
+
+//             if (entry.isIntersecting) {
+//               // Animate ONLY when scrolling DOWN
+//               if (currentScrollY > lastScrollY) {
+//                 entry.target.classList.add("show");
+//               }
+//             } else {
+//               // Reset silently when scrolling UP
+//               if (currentScrollY < lastScrollY) {
+//                 entry.target.classList.remove("show");
+//               }
+//             }
+
+//             lastScrollY = currentScrollY;
+//           });
+//         },
+//         {
+//           threshold: 0.3,
+//         },
+//       );
+
+//       elements.forEach((el) => observer.observe(el));
+//     }
+//   }
+
+//   //SECTION FOUR ANIMATION
+//   {
+//     {
+//       //TEXT
+//       const elements = document.querySelectorAll(".section-four-animation-text");
+
+//       let lastScrollY = window.scrollY;
+
+//       const observer = new IntersectionObserver(
+//         (entries) => {
+//           entries.forEach((entry) => {
+//             const currentScrollY = window.scrollY;
+
+//             if (entry.isIntersecting) {
+//               // animate only when scrolling DOWN
+//               if (currentScrollY > lastScrollY) {
+//                 entry.target.classList.add("show");
+//               }
+//             } else {
+//               // reset silently when scrolling UP
+//               if (currentScrollY < lastScrollY) {
+//                 entry.target.classList.remove("show");
+//               }
+//             }
+
+//             lastScrollY = currentScrollY;
+//           });
+//         },
+//         {
+//           threshold: 0.3,
+//         },
+//       );
+
+//       elements.forEach((el) => observer.observe(el));
+//     }
+//     {
+//       //IMAGE
+//       const elements = document.querySelectorAll(".section-four-animation-image");
+
+//       let lastScrollY = window.scrollY;
+
+//       const observer = new IntersectionObserver(
+//         (entries) => {
+//           entries.forEach((entry) => {
+//             const currentScrollY = window.scrollY;
+
+//             if (entry.isIntersecting) {
+//               // animate only when scrolling DOWN
+//               if (currentScrollY > lastScrollY) {
+//                 entry.target.classList.add("show");
+//               }
+//             } else {
+//               // reset when scrolling UP
+//               if (currentScrollY < lastScrollY) {
+//                 entry.target.classList.remove("show");
+//               }
+//             }
+
+//             lastScrollY = currentScrollY;
+//           });
+//         },
+//         {
+//           threshold: 0.3,
+//         },
+//       );
+
+//       elements.forEach((el) => observer.observe(el));
+//     }
+//   }
+
+//   //SECTION FIVE
+//   {
+//     {
+//       const elements = document.querySelectorAll(
+//         ".section-five-text-animation, .section-five-images-animation",
+//       );
+
+//       let lastScrollY = window.scrollY;
+
+//       const observer = new IntersectionObserver(
+//         (entries) => {
+//           entries.forEach((entry) => {
+//             const currentScrollY = window.scrollY;
+
+//             if (entry.isIntersecting) {
+//               // animate only when scrolling DOWN
+//               if (currentScrollY > lastScrollY) {
+//                 entry.target.classList.add("show");
+//               }
+//             } else {
+//               // reset only when scrolling UP
+//               if (currentScrollY < lastScrollY) {
+//                 entry.target.classList.remove("show");
+//               }
+//             }
+
+//             lastScrollY = currentScrollY;
+//           });
+//         },
+//         {
+//           threshold: 0.3,
+//         },
+//       );
+
+//       elements.forEach((el) => observer.observe(el));
+//     }
+//   }
+// }
+//NAV BAr TOGGLE
+
 {
-  //HEADING
-  {
-    const elements = document.querySelectorAll(
-      ".section-two-heading-animation",
-    );
+  const menuBtn = document.getElementById("menuBtn");
+  const sideMenu = document.getElementById("sideMenu");
+  const closeBtn = document.getElementById("closeMenu");
 
-    let lastScrollY = window.scrollY;
+  // Open menu
+  menuBtn.addEventListener("click", () => {
+    sideMenu.classList.add("active");
+  });
 
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          const currentScrollY = window.scrollY;
+  // Close menu
+  closeBtn.addEventListener("click", () => {
+    sideMenu.classList.remove("active");
+  });
 
-          if (entry.isIntersecting) {
-            // Only animate when scrolling DOWN
-            if (currentScrollY > lastScrollY) {
-              entry.target.classList.add("show");
-            }
-          } else {
-            // Reset only when scrolling UP (so it can animate again later)
-            if (currentScrollY < lastScrollY) {
-              entry.target.classList.remove("show");
-            }
-          }
-
-          lastScrollY = currentScrollY;
-        });
-      },
-      {
-        threshold: 0.3,
-      },
-    );
-
-    elements.forEach((el) => observer.observe(el));
-  }
-
-  {
-    //SECOND SECTION CASE STUDY
-    const elements = document.querySelectorAll(
-      ".section-two-second-div-animation",
-    );
-
-    let lastScrollY = window.scrollY;
-
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          const currentScrollY = window.scrollY;
-
-          if (entry.isIntersecting) {
-            // animate only when scrolling DOWN
-            if (currentScrollY > lastScrollY) {
-              entry.target.classList.add("show");
-            }
-          } else {
-            // reset when scrolling UP
-            if (currentScrollY < lastScrollY) {
-              entry.target.classList.remove("show");
-            }
-          }
-
-          lastScrollY = currentScrollY;
-        });
-      },
-      {
-        threshold: 0.3,
-      },
-    );
-
-    elements.forEach((el) => observer.observe(el));
-  }
 }
 
-//SECTION THREE ANIMATION
+//sidebar active code
 {
-  //1ST
-  {
-    const elements = document.querySelectorAll(".section-three-animation-one");
+  const links = document.querySelectorAll(".side-menu-anchor");
 
-    let lastScrollY = window.scrollY;
+  links.forEach(link => {
+    link.addEventListener("click", () => {
 
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          const currentScrollY = window.scrollY;
+      links.forEach(l => l.classList.remove("active"));
 
-          if (entry.isIntersecting) {
-            // Animate ONLY when scrolling DOWN
-            if (currentScrollY > lastScrollY) {
-              entry.target.classList.add("show");
-            }
-          } else {
-            // Reset silently when scrolling UP
-            if (currentScrollY < lastScrollY) {
-              entry.target.classList.remove("show");
-            }
-          }
 
-          lastScrollY = currentScrollY;
-        });
-      },
-      {
-        threshold: 0.3,
-      },
-    );
-
-    elements.forEach((el) => observer.observe(el));
-  }
-  {
-    //second
-    const elements = document.querySelectorAll(".section-three-animation-one");
-
-    let lastScrollY = window.scrollY;
-
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          const currentScrollY = window.scrollY;
-
-          if (entry.isIntersecting) {
-            // Animate ONLY when scrolling DOWN
-            if (currentScrollY > lastScrollY) {
-              entry.target.classList.add("show");
-            }
-          } else {
-            // Reset silently when scrolling UP
-            if (currentScrollY < lastScrollY) {
-              entry.target.classList.remove("show");
-            }
-          }
-
-          lastScrollY = currentScrollY;
-        });
-      },
-      {
-        threshold: 0.3,
-      },
-    );
-
-    elements.forEach((el) => observer.observe(el));
-  }
-}
-
-//SECTION FOUR ANIMATION
-{
-  {
-    //TEXT
-    const elements = document.querySelectorAll(".section-four-animation-text");
-
-    let lastScrollY = window.scrollY;
-
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          const currentScrollY = window.scrollY;
-
-          if (entry.isIntersecting) {
-            // animate only when scrolling DOWN
-            if (currentScrollY > lastScrollY) {
-              entry.target.classList.add("show");
-            }
-          } else {
-            // reset silently when scrolling UP
-            if (currentScrollY < lastScrollY) {
-              entry.target.classList.remove("show");
-            }
-          }
-
-          lastScrollY = currentScrollY;
-        });
-      },
-      {
-        threshold: 0.3,
-      },
-    );
-
-    elements.forEach((el) => observer.observe(el));
-  }
-  {
-    //IMAGE
-    const elements = document.querySelectorAll(".section-four-animation-image");
-
-    let lastScrollY = window.scrollY;
-
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          const currentScrollY = window.scrollY;
-
-          if (entry.isIntersecting) {
-            // animate only when scrolling DOWN
-            if (currentScrollY > lastScrollY) {
-              entry.target.classList.add("show");
-            }
-          } else {
-            // reset when scrolling UP
-            if (currentScrollY < lastScrollY) {
-              entry.target.classList.remove("show");
-            }
-          }
-
-          lastScrollY = currentScrollY;
-        });
-      },
-      {
-        threshold: 0.3,
-      },
-    );
-
-    elements.forEach((el) => observer.observe(el));
-  }
-}
-
-//SECTION FIVE
-{
-  {
-    const elements = document.querySelectorAll(
-      ".section-five-text-animation, .section-five-images-animation",
-    );
-
-    let lastScrollY = window.scrollY;
-
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          const currentScrollY = window.scrollY;
-
-          if (entry.isIntersecting) {
-            // animate only when scrolling DOWN
-            if (currentScrollY > lastScrollY) {
-              entry.target.classList.add("show");
-            }
-          } else {
-            // reset only when scrolling UP
-            if (currentScrollY < lastScrollY) {
-              entry.target.classList.remove("show");
-            }
-          }
-
-          lastScrollY = currentScrollY;
-        });
-      },
-      {
-        threshold: 0.3,
-      },
-    );
-
-    elements.forEach((el) => observer.observe(el));
-  }
+      link.classList.add("active");
+    });
+  });
 }
